65e7ca16341c51966962b1ddedf78a3c400b1971
MonkeyKingYY <1297348858@qq.com>
2021-09-15 07:19:16 +0800

    basci-type,interface-basic,array-and-tuple

---
+++ array-and-tuple.ts
@@ -0,0 +1,9 @@
+let arrOfNumbers: number[] = [1, 2, 3]
+arrOfNumbers.push(4)
+
+function test() {
+    console.log(arguments);
+}
+
+// 元组
+let user: [string, number] = ["yee", 20]
---
+++ interface-basic.ts
@@ -0,0 +1,11 @@
+interface Person {
+    readonly id: number,
+    name: string,
+    age?: number
+}
+
+let yee: Person = {
+    id: 1,
+    name: 'yee',
+    age: 20
+}
